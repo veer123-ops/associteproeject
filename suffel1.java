a641b09429527336502301de6c24d92cbc827e58
veer123-ops <72941982+veer123-ops@users.noreply.github.com>
2023-10-22 16:20:03 +0530

    Add files via upload

---
+++ suffel1.java
@@ -0,0 +1,38 @@
+package veer.com;
+
+//public class suffel1 {
+
+	
+	
+	import java.util.*;
+	import java.util.concurrent.ThreadLocalRandom;
+
+	public class suffel1 
+	{
+	  public static void main(String args[])
+	  {
+	    int[] solutionArray = {1, 2, 3, 4, 5, 6, 7 };
+
+	    shuffleArray(solutionArray);
+	    for (int i = 0; i < solutionArray.length; i++)
+	    {
+	      System.out.print(solutionArray[i] + " ");
+	    }
+	    System.out.println();
+	  }
+
+	  // Implementing Fisher–Yates shuffle
+	  static void shuffleArray(int[] ar)
+	  {
+	    // If running on Java 6 or older, use `new Random()` on RHS here
+	    Random rnd = ThreadLocalRandom.current();
+	    for (int i = ar.length - 1; i > 0; i--)
+	    {
+	      int index = rnd.nextInt(i + 1);
+	      // Simple swap
+	      int a = ar[index];
+	      ar[index] = ar[i];
+	      ar[i] = a;
+	    }
+	  }
+}
